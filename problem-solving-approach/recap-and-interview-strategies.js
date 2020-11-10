e4c404feda79cc2d5182ebcab045d55de4a57be8
castle <castlesmadeofcode@gmail.com>
2020-11-10 13:32:18 -0600

    Section 4: 23 - Recap and Interview Strategies

---
+++ problem-solving-approach/recap-and-interview-strategies.js
@@ -0,0 +1,14 @@
+
+// Section 4: 23 - Recap and Interview Strategies
+
+// RECAP!
+// Understand the Problem
+// Explore Concrete Examples
+// Break It Down
+// Solve/Simplify
+// Look Back and Refactor
+
+// HOW DO YOU IMPROVE?
+// Devise a plan for solving problems
+// Master common problem solving patterns
+// Devise a plan for solving problems
