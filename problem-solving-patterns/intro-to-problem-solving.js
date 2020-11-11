1e732a251891deeed5bbecea957b719fdaef1b3f
castle <castlesmadeofcode@gmail.com>
2020-11-11 14:09:36 -0600

    Section 5:27 - Frequency Counter Pattern

---
+++ problem-solving-patterns/intro-to-problem-solving.js
@@ -0,0 +1,13 @@
+// Section 5:26 - Intro to Problem Solving Patterns
+
+// PROBLEM SOLVING PATTERNS
+
+// SOME PATTERNS...
+// Frequency Counter
+// Multiple Pointers
+// Sliding Window
+// Divide and Conquer
+// Dynamic Programming
+// Greedy Algorithms
+// Backtracking
+// Many more!
